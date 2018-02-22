8e346312cd7b42ff9414364a2e276eb951c334b2
Jeffrey Costa <cosjef@jeffreys-imac.attlocal.net>
2018-02-21 23:46:48 -0500

    after finishing chapter 11

--- models/user.js
+++ models/user.js
@@ -5,7 +5,14 @@ var Schema = mongoose.Schema;
 
 
 var userSchema = new mongoose.Schema({
-    email: { type: String, required: true },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        // validate that the email address is in a good format with a regex
+        // regex not working 
+        // match: /[a - z0 - 9!#$ %& '*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&' * +/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+    },
     password: { type: String, required: true }
 }, { timestamps: {} });
 
