7d2f4e3f5fe8fc00c25a94d6138bc1a45df463d4
Jeff Costa <cosjef@gmail.com>
2018-05-20 23:17:12 -0400

    turned caching on

--- routes/catRoutes.js
+++ routes/catRoutes.js
@@ -24,8 +24,8 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 // add caching
-// const apicache = require('apicache');
-// const cache = apicache.middleware;
+const apicache = require('apicache');
+const cache = apicache.middleware;
 
 // =======================================================================================================================
 // Routes are listed below
@@ -35,8 +35,8 @@ router.use(bodyParser.json());
 router.post('/', catController.cat_create_cat);
 router.get('/:id', catController.get_one_cat);
 router.put('/:id', catController.update_a_cat);
-router.get('/', catController.get_all_cats);
-//router.get('/', cache('5 minutes'), catController.get_all_cats);
+//router.get('/', catController.get_all_cats);
+router.get('/', cache('5 minutes'), catController.get_all_cats);
 router.delete('/:id', catController.delete_a_cat);
 router.post('/import', catController.import_cats);
 
